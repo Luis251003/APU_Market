14c3f1fbd0f6e99f473ebc3a3f6bed915b9cebd3
Luis251003 <luisortiz251003@gmail.com>
2025-07-21 21:49:59 -0500

    Implementación de Thymeleaf
    Mejorando el servicio de mensajería con thymeleaf

--- App/BackEnd/apu-market/src/main/java/com/apu/market/apu_market/services/EmailService.java
+++ App/BackEnd/apu-market/src/main/java/com/apu/market/apu_market/services/EmailService.java
@@ -2,13 +2,18 @@ package com.apu.market.apu_market.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.apu.market.apu_market.dto.EmailDTO;
 import com.apu.market.apu_market.errors.EmailException;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -16,19 +21,36 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Value("${spring.mail.username}")
     private String emailFrom;
 
     public String enviarCorreo(EmailDTO emailDTO){
         try{
-            SimpleMailMessage mensaje = new SimpleMailMessage();
-            mensaje.setTo(emailDTO.getPara());
-            mensaje.setSubject(emailDTO.getAsunto());
-            mensaje.setText(emailDTO.getMensaje());
-            mensaje.setFrom(emailFrom);
+
+            //Preparar HTML con Thymeleaf
+            Context context = new Context();
+            context.setVariable("codigo", "54321");
+            String htmlContent = templateEngine.process("email/plantilla-codigo-seguridad", context);
+
+            //Crear el correo con contenido HTML
+            MimeMessage mensaje = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mensaje, true, "UTF-8");
+
+            helper.setTo(emailDTO.getPara());
+            helper.setSubject(emailDTO.getAsunto());
+            helper.setFrom(emailFrom);
+            helper.setText(htmlContent, true);
+
+            // Adjuntar imagen embebida
+            // ClassPathResource imagen = new ClassPathResource("/static/images/logo_apu_core.png");
+            // helper.addInline("logoCorreo", imagen); // "logoCorreo" debe coincidir con el cid del HTML
+
             javaMailSender.send(mensaje);
-            return "Correo enviado con éxito";
-        }catch(MailException ex){
+            return "Correo HTML enviado correctamente.";
+        }catch(MessagingException ex){
             System.err.println(ex.getMessage());
             throw new EmailException(ex.getMessage());
         }
